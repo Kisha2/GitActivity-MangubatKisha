c251a07696ba54084becaf19b82b88779cc57fa0
MangubatKisha <mangubatkisha02@gmail.com>
2023-03-09 14:45:04 +0800

    My Educational Background added

--- git-python-mangubat.py
+++ git-python-mangubat.py
@@ -2,4 +2,6 @@
 print ("Kisha J. Mangubat")
 print ("Tugos, Boac, Marinduque")
 print ("July 2, 2003")
-
+print ("College of Information and Computing Sciences")
+print ("Bachelor of Science in Information Technology")
+print ("2A")
