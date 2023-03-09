32b479f0e981060715ccbda94071068f90b989c8
MangubatKisha <mangubatkisha02@gmail.com>
2023-03-09 14:57:47 +0800

    My Hobbies added

--- git-python-mangubat.py
+++ git-python-mangubat.py
@@ -5,3 +5,8 @@ print ("July 2, 2003")
 print ("College of Information and Computing Sciences")
 print ("Bachelor of Science in Information Technology")
 print ("2A")
+hobbies = ["Singing", "Watching Movies", "Cooking", "Playing Outdoor Games", "Reading Fictional Books"]
+print ("")
+print ("MY HOBBIES")
+print (*hobbies, sep = "\n")
+
